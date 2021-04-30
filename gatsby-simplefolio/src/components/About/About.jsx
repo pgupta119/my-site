7322edc6f7bb572cd42dc2358a70f17868383f86
pgupta119 <guptaprakash1995119@gmail.com>
2021-05-01 01:26:17 +0200

    website files

---
+++ gatsby-simplefolio/src/components/About/About.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    " My name is Prakash Gupta and from India. Currently, I am pursuing my master's in Data science for decison making at Maastricht University in Netherlands. "}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    " I am a enthusiast about  Machine Learning, data and programming. I also do running, playing sports cricket, footballa and badmintion. I strongly believe in team work and inidividual rolex§ "}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || '“With too little data, you won’t be able to make any conclusions that you trust. With loads of data you will find relationships that aren’t real… Big data isn’t about bits, it’s about talent.'}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={'https://drive.google.com/file/d/1LVjrMch9BAja_QtuTW7BVInyMLWdAnwP/view?usp=sharing'}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ gatsby-simplefolio/src/mock/data.js
@@ -0,0 +1,95 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Hello, my name is ',
+  name: 'Prakash Gupta',
+  subtitle: "I'm  Master's Student ",
+  cta: 'Know More',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile1.jpeg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'churn.png',
+    title: ' Churn Problem',
+    info: 'The problem of churn is a problem when a number of individuals move out of a collective group. It is one of the main problems that determine the steady-state level of customers in any type of business.',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/pgupta119/Churn-Problem', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'credit.png',
+    title: 'Credit-Card-Fraud-detection',
+    info: 'credit card companies are able to recognize fraudulent credit card transactions so that customers are not charged for items that they did not purchase. ',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/pgupta119/Credit-Card-Faurd-Detection-kaggle--dataset', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'recommend.jpeg',
+    title: 'Recommender-System',
+    info: '  The basic recommender-system for user to get movie recommendation',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/pgupta119/Recommender-System', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Euro.jpeg',
+    title: 'EURO-2021 Prediction',
+    info: ' This project is to predict the Euro Cup Winner of 2021 by using Machine learning techniques and Data sets.',
+    info2: ' currently working',
+    url: '',
+    repo: 'https://github.com/pgupta119/Euro-Cup-Prediction', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '+31626549536',
+  btn: '',
+  email: "p.guptaprakash94@gmail.com",
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      title:'linkedon',
+      name: 'linkedin',
+      url: 'https://linkedin.com/in/prakashgupta119',
+    },
+    {
+      id: nanoid(),
+      title:'github',
+      name: 'github',
+      url: 'https://github.com/pgupta119',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
